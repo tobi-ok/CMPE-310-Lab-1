233ba96a02235a3377f0dba390b42055ba5449db
Tobi Ok <146225328+tobi-ok@users.noreply.github.com>
2026-02-27 03:40:33 +0000

    complete task 1

--- Lab2/task1.s
+++ Lab2/task1.s
@@ -1,10 +1,4 @@
-.section .data
-prompt1: .asciz "Enter first string: "
-prompt2: .asciz "Enter second string: "
-
-.section .bss
-.lcomm str1, 256        # Hold 256 bytes for string 1
-.lcomm str2, 256        # Hold 256 bytes for string 2
+# use c to get string input
 
 .section .text
 .globl start_task
@@ -12,17 +6,20 @@ prompt2: .asciz "Enter second string: "
 start_task:
     movl $str1, %esi        # Point to string 1
     movl $str2, %edi        # Point to string 2
-    movw $0, %dx            # Hamming Distance counter
+    movl $0, %edx            # Hamming Distance counter
 
 compare_loop:
-    cmp $0, %ecx         # Check if counter is 0
-    je results          # Jump to results if equal to 0
-
-    # Else
-
     movb (%esi), %al
     movb (%edi), %bl
-    xorb %al, %bl
+
+    cmp $0, %al         # Check if string 1 is done
+    je results          # Jump to results if equal to 0
+    cmp $0, %bl         # Check if string 2 is done
+    je results          # Jump to results if equal to 0
+
+    # Both strings still have text
+    
+    xorb %bl, %al
 
     # 0000 0000
     # Test each bit to the xor results
@@ -30,51 +27,50 @@ compare_loop:
 
     testb $1, %al
     jz b2
-    inc %dx
+    inc %edx
 
 b2:
     testb $2, %al
     jz b3
-    inc %dx
+    inc %edx
 
 b3:
     testb $4, %al
     jz b4
-    inc %dx
+    inc %edx
 
 b4:
     testb $8, %al
     jz b5
-    inc %dx
+    inc %edx
     
 b5:
     testb $16, %al
     jz b6
-    inc %dx
+    inc %edx
 
 b6:
     testb $32, %al
     jz b7
-    inc %dx
+    inc %edx
 
 b7:
     testb $64, %al
     jz b8
-    inc %dx
+    inc %edx
     
 b8:
     testb $128, %al
     jz next_char
-    inc %dx
+    inc %edx
 
 next_char:
     inc %esi
     inc %edi
-    dec %ecx
     jmp compare_loop
 
 results:
-    mov $1, %eax
-    mov $0, %ebx
+    mov %edx, result        # Put results in result variable in c
+    ret
 
 .section .note.GNU-stack,"",@progbits
--- Lab2/test_ham.c
+++ Lab2/test_ham.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-extern unsigned char ram[]; // RAM declared in
-extern void start_task(void); // Assembly function
+char str1[256];
+char str2[256];
+int result = 0;
 
-int main()
-{
-    start_task(); // Run assembly code    
+extern void start_task(void); // Link assembly function
+
+int main() {
+    printf("Enter first string: ");
+    scanf("%s", str1);
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    start_task(); // Jump to assembly logic
+
+    printf("Hamming Distance: %d\n", result);
     return 0;
 }
